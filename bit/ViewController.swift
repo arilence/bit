ed4d041ba8a0da0e2504dfd0d23b8f2084e294fd
Anthony Smith <anthony@smith.lt>
2016-02-03 19:36:22 -0800

    Initial Commit

---
+++ bit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bit
+//
+//  Created by Anthony on 2016-02-03.
+//  Copyright © 2016 Anthony Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
