34cfdf69abc39b2696d72672f47acacc2cacb224
Anthony Smith <anthony@smith.lt>
2016-03-10 17:06:08 -0800

    Refactor settings keys and values to seperate class

--- bit/SettingsController.swift
+++ bit/SettingsController.swift
@@ -1,5 +1,5 @@
 //
-//  CurrencyController.swift
+//  SettingsController.swift
 //  bit
 //
 //  Created by Anthony on 2016-03-08.
@@ -25,7 +25,37 @@ class SettingsController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func saveButton(sender: AnyObject) {
+        // TODO: Save curreny preference
+        closeView()
+    }
+    
     @IBAction func closeButton(sender: AnyObject) {
+        closeView()
+    }
+    
+    // Set the font colour of the elements in the pickerView to white
+    func pickerView(pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+        let titleData = Settings.settingsCurrency[row]
+        let myTitle = NSAttributedString(string: titleData, attributes: [NSFontAttributeName:UIFont(name: "Helvetica", size: 15.0)!,NSForegroundColorAttributeName:UIColor.whiteColor()])
+        return myTitle
+    }
+    
+    // Resizes the height of each element in pickerView
+    func pickerView(pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 48.0
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return Settings.settingsCurrency.count
+    }
+    
+    // Returns the user back to the home page
+    func closeView() {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
